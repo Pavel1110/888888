5c19e34a53e784acb869d8659d9ddfe3012d1e46
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-21 00:14:05 +0300

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число: ");
+int a = int.Parse (Console.ReadLine()!);
+if (a%2==0)
+{
+    Console.WriteLine ("четное число");
+}
+else
+{
+   Console.WriteLine ("нечетное число");
+}
