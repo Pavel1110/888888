39780b798f7db7041445615ad79f1369a1b7f053
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-21 00:26:11 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 100 * 10 + a % 10;
+Console.WriteLine(b);
