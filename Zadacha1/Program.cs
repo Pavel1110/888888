eb1a5148b4c447bf812861cc1bb51e4c02f673e6
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-20 23:25:28 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("проверяем является ли первое число квадратом второго");
+if (a == b*b)
+{
+    Console.Write ("число a является квадратом числа b");
+}
+else
+{
+    Console.Write ("число a не является квадратом числа b");
+}
