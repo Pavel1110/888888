acca13f506d36b8502333f2892f882b9564e6f41
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-21 00:28:02 +0300

    Zadacha9

---
+++ Zadacha9/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+Double FN = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double SN = Double.Parse(Console.ReadLine()!);
+
+
+   if ( FN % SN == 0 )
+   {
+        Console.Write("Первое число кратно второму");
+   }
+   else
+   {
+    Console.WriteLine(FN/SN);
+    Console.WriteLine("Число не кратно");
+   }
