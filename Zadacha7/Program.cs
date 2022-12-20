e24199855b54cb1e93feb299501a9c90d239274d
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-21 00:23:27 +0300

    Zadacha7

---
+++ Zadacha7/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = new Random().Next(10, 99);
+Console.WriteLine(numberA);
+
+if (numberA > 10 && numberA < 99)
+{
+if (numberA / 10 > numberA % 10)
+{
+        Console.WriteLine("Первая цифра больше");
+}
+else
+if (numberA / 10 == numberA % 10)
+{
+        Console.WriteLine("Цифры одинаковы");
+}
+else
+Console.WriteLine("Вторая цифра больше");
+}
