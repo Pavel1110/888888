f677726c2cc1b3cf7ad4a5ce299ee92a11945c52
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-22 10:41:29 +0300

    Zadacha11

---
+++ Zadacha11/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 100 % 10;
+if ( b % 10 > 0  )
+{
+    Console.Write("Третье число ");
+    Console.WriteLine(b);
+}
+else {
+    Console.WriteLine("Третьего числа нет");
+ }
