68a1b955e2e0310fe1ba6386bea2f3eb08157049
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-22 10:46:23 +0300

    Zadacha12

---
+++ Zadacha12/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Введите число номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+if(day == a)
+{
+    Console.WriteLine ("понедельник");
+    Console.WriteLine ("Будний день!");
+}
+
+if(day == b)
+{
+    Console.WriteLine ("вторник");
+    Console.WriteLine ("Будний день!");
+}
+if(day == c)
+{
+    Console.WriteLine ("среда");
+    Console.WriteLine ("Будний день!");
+}
+if(day == d)
+{
+    Console.WriteLine ("четверг");
+    Console.WriteLine ("Будний день!");
+}
+if(day == e)
+{
+    Console.WriteLine ("пятница");
+    Console.WriteLine ("Будний день!");
+}
+if(day == f)
+{
+    Console.WriteLine ("суббота");
+    Console.WriteLine ("Сегодня выходной!");
+}
+if(day == g)
+{
+    Console.WriteLine ("воскресенье");
+    Console.WriteLine ("Сегодня выходной!");
+}
