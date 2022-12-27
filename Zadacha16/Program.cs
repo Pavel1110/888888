8d301a658242e22bba6098286c6d17a9665acc1c
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-27 16:21:11 +0300

    .\Zadacha16\

---
+++ Zadacha16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число A");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число B");
+int b = int.Parse(Console.ReadLine()!);
+int chislo = a;
+int i = 1;
+while (i < b)
+{
+chislo = chislo * a;
+i++;
+}
+Console.WriteLine("A в степени B равно: " + chislo);
