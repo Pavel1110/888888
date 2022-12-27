f901c2055af245b3147ea244df7e3debec283e23
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-27 16:26:03 +0300

    .\Zadacha18\

---
+++ Zadacha18/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Будет создан массив длиной A в диапвзоне 0-A");
+Console.Write("Введите любое число (A): ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[x];
+
+for (int i = 0; i < x; i++)
+{
+ array[i] = new Random().Next(x+1);
+ Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
